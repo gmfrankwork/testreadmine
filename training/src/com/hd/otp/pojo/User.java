a1d2b0d4ee3f288d77a702be157966aecc41c508
ksfrankdou0423 <frankdou0423@126.com>
2017-02-24 09:34:58 +0800

    finish #4 "添加password属属性"

--- training/src/com/hd/otp/pojo/User.java
+++ training/src/com/hd/otp/pojo/User.java
@@ -5,6 +5,16 @@ public class User {
     
     private String name;
 
+    private String password;
+    
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public int getId() {
 		return id;
 	}
