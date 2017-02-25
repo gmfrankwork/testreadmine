c043972351c363ba24184e5f0705b076f1fcc406
ksfrankdou0423 <frankdou0423@126.com>
2017-02-25 12:09:42 +0800

    ok:#123

--- training/src/com/hd/otp/pojo/User.java
+++ training/src/com/hd/otp/pojo/User.java
@@ -14,6 +14,17 @@ public class User implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private int age;
+	
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	private int id;
     
